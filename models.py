9d81c46fb7154615e54b9625e3850d71a9bf85cb
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-04-24 12:41:26 -0300

    feat(user): class de usuario

---
+++ models.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+
+class Sexo(str, Enum):
+    masculino = "masculino"
+    feminino = "feminino"
+    
+class Regras(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student"
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    nome: str
+    sobrenome: str
+    sexo: Sexo
+    regras: List[Regras]
